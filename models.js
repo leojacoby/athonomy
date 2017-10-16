59acdb488806494c709d87d57c1cad2087bcce8a
leojacoby <leo_jacoby@branson.org>
2017-10-16 12:49:43 -0700

    updated models and added babel

--- models.js
+++ models.js
@@ -1,15 +1,15 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var userSchema = mongoose.Schema({
-  username: String,
-  password: String,
-  phone: String
+const userSchema = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
 });
 
-
-
-User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    User:User
+    User: User
 };
