edcb249c182b6f87ac71059e65e744c946f012d2
leojacoby <leo_jacoby@branson.org>
2017-10-22 16:57:17 -0700

    models now has pledge, participants, routes to join events from event.hbs

--- models.js
+++ models.js
@@ -12,18 +12,71 @@ const eventSchema = new Schema({
   name: {type: String, required: true},
   organizer: {type: String, required: true},
   date: {type: Date, required: true},
+  time: {type: String, required: true},
   location: {type: String, required: true},
   description: {type: String, required: true},
   cause: {type: String, required: true},
   private: {type: Boolean, required: true},
-  measurement: {type: String, required: true}
-})
+  measurement: {type: String, required: true},
+  participants: [{
+    type: Schema.ObjectId,
+    ref: "Participant",
+    default: [],
+    required: true
+  }],
+  admin: {
+    type: Schema.ObjectId,
+    ref: "User",
+    required: true
+  }
+});
+
+const participantSchema = new Schema({
+  participant: {
+    type: Schema.ObjectId,
+    ref: "User",
+    required: true
+  },
+  event: {
+    type: Schema.ObjectId,
+    ref: "Event",
+    required: true
+  },
+  joinTime: {type: Date, required: true},
+  pledges: [{
+    type: Schema.ObjectId,
+    ref: "Pledge",
+    required: true
+  }]
+});
+
+const pledgeSchema = new Schema({
+  participant: {
+    type: Schema.ObjectId,
+    ref: "Participant",
+    required: true
+  },
+  event: {
+    type: Schema.ObjectId,
+    ref: "Event",
+    required: true
+  },
+  pledgeTime: {type: Date, required: true},
+  pledgeAmount: {type: Number, required: true},
+  pledgePaid: {type: Boolean, default: false, required: true}
+});
 
 const User = mongoose.model('User', userSchema);
 
 const Event = mongoose.model('Event', eventSchema);
 
+const Participant = mongoose.model('Participant', participantSchema);
+
+const Pledge = mongoose.model('Pledge', pledgeSchema);
+
 module.exports = {
-    User: User,
-    Event: Event
+  User: User,
+  Event: Event,
+  Participant: Participant,
+  Pledge: Pledge
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var User = models.User;
 var Event = models.Event;
+var Participant = models.Participant;
+var Pledge = models.Pledge;
 
 //////////////////////////////// PUBLIC ROUTES ////////////////////////////////
 // Users who are not logged in can see these routes
@@ -10,6 +13,28 @@ router.get('/', (req, res, next) => {
   res.render('home');
 });
 
+router.get('/events/:id', (req, res) => {
+  console.log("got /events/:id")
+  Event.findById(req.params.id)
+  .then((event, err) => {
+    if (err) {
+      console.log("Error accessing event", err)
+      res.status(500);
+    } else if (!event) {
+      console.log("Event not found")
+      res.redirect('/');
+    } else {
+      console.log("Successfully got event, rendering...")
+      let dateString = event.date.toString().split(' ');
+      dateString.splice(4, 2);
+      dateString = dateString.join(' ');
+      let datetimeString = event.time + ' ' + dateString;
+      res.render('event', {
+        event: Object.assign(event, {datetimeString: datetimeString})
+      });
+    }
+  })
+})
 ///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
 
 router.use((req, res, next) => {
@@ -30,7 +55,8 @@ router.get('/newevent', (req, res, next) => {
 });
 
 router.post('/newevent', (req, res, next) => {
-  let {name, organizer, date, location, description, cause, private, measurement} = req.body;
+  console.log("Post to /newevent");
+  let {name, organizer, date, time, location, description, cause, private, measurement} = req.body;
   if (private === "true") {
     private = true;
   } else {
@@ -40,11 +66,13 @@ router.post('/newevent', (req, res, next) => {
     name,
     organizer,
     date,
+    time,
     location,
     description,
     cause,
     private,
-    measurement
+    measurement,
+    admin: req.user._id
   }).save((err, event) => {
     if (err) {
       console.log("Error saving new event", err);
@@ -52,6 +80,66 @@ router.post('/newevent', (req, res, next) => {
     } else {
       console.log("New event saved!")
       res.status(200).redirect('/events/'+event._id);
+    }
+  })
+})
+
+router.get('/events/admin/:id', (req, res) => {
+  Event.findById(req.params.id)
+  .populate("admin")
+  .then((event, err) => {
+    if (err) {
+      console.log("Error accessing event", err)
+      res.status(500);
+    } else if (!event) {
+      console.log("Event not found")
+      res.redirect('/');
+    } else {
+      if (String(event.admin._id) === String(req.user._id)) {
+        console.log("Successfully got event admin page, rendering...");
+        let dateString = event.date.toString().split(' ');
+        dateString.splice(4, 2);
+        dateString = dateString.join(' ');
+        let datetimeString = event.time + ' ' + dateString;
+        // TODO: render a different admin page with access to all participants
+        res.render('event', {
+          event: Object.assign(event, {datetimeString: datetimeString})
+        });
+      } else {
+        // boot them back
+        console.log('event.admin._id', event.admin._id);
+        console.log('req.user._id', req.user._id);
+        res.redirect('/');
+      }
+    }
+  })
+})
+
+router.post('/joinevent/:id', (req, res) => {
+  // join event
+  new Participant({
+    participant: req.user._id,
+    event: req.params.id,
+    joinTime: new Date(),
+  }).save((err, participant) => {
+    if (err) {
+      console.log("Could not save new participant", err);
+    } else {
+      Event.update({_id: req.params.id}, {$push:{participants: participant._id}}, (err, update) => {
+        if (err) {
+          console.log("Could not update event with new participant", err);
+          res.send(err)
+        } else if (!update) {
+          console.log("something went wrong!", event)
+        } else {
+          console.log("New event updated with new participant!", update);
+          // TODO: redirect to participant admin page
+          res.redirect('/events/'+req.params.id);
+        }
+      })
+
+      console.log("New participant saved!");
+      // redirect to participant admin page
     }
   })
 })
